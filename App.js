05b82a0db0c5a5778fa1d9c98382afb5e84f5e3a
Juliette Rapala <juliette@eventric.com>
2019-06-26 14:51:21 -0500

    Move restaurants to constants file

--- App.js
+++ App.js
@@ -2,29 +2,7 @@ import React, {Component} from 'react'
 import {FlatList, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native'
 import Header from 'components/Header'
 import RestaurantRow from 'components/RestaurantRow'
-
-const restaurants = [
-	{name: 'React Cafe', address: '123 Anywhere Street'},
-	{name: 'Fancy Restaurant', address: '799 Main Street'},
-	{name: 'Taco Place', address: '550 Maple Road'},
-	{name: "Tony's Diner", address: '4104 College St'},
-	{name: 'Pasta Central', address: '706 Central St'},
-	{name: 'Burger Builder', address: '4869 Hamilton Dr'},
-	{name: 'Pizza Express', address: '1049 Bird St'},
-	{name: 'Teriyaki To Go', address: '1885 Tea Berry Ln'},
-	{name: 'Maroon Deli', address: '1082 Stuart St'},
-	{name: 'Prime Bar and Grill', address: '1848 Fairfax Dr'},
-	{name: 'Dumpling House', address: '747 Kelly Dr'},
-	{name: 'Hot Chicken', address: '1816 Olive St'},
-	{name: "Luna's Tap Room", address: '3256 Spirit Dr'},
-	{name: 'Quick Sandwich Shop', address: '2587 Cherry Ridge Dr'},
-	{name: "Bobby's Burgers", address: '4152 Berkley St'},
-	{name: 'Turnpike Diner', address: '4571 Central Ave'},
-	{name: 'Bombay Express', address: '65 Queens Ln'},
-	{name: 'Coffee Central', address: '3228 Oakwood Cr'},
-	{name: "King's Garden", address: '2935 Victoria Ct'},
-	{name: 'Salads and More', address: '2454 Preston St'},
-]
+import {restaurants} from './lib/constants'
 
 export default class App extends Component<Props> {
 	state = {
