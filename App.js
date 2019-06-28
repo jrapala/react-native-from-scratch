07632a7e0b582cba0b0642ef606afac5624953fe
Juliette Rapala <juliette@eventric.com>
2019-06-28 06:47:29 -0500

    Add bottom tab navigation

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import React, {Component} from 'react'
-import {createStackNavigator, createAppContainer} from 'react-navigation'
+import {createStackNavigator, createAppContainer, createBottomTabNavigator} from 'react-navigation'
 import RestaurantList from 'components/RestaurantList'
 import RestaurantInfo from 'components/RestaurantInfo'
+import About from 'components/About'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
-const AppNavigator = createStackNavigator(
+const List = createStackNavigator(
 	{
 		Home: {screen: RestaurantList},
 		Info: {screen: RestaurantInfo},
 	},
 	{
-		navigationOptions: {
-			title: 'Restaurant Info',
+		defaultNavigationOptions: {
 			headerStyle: {
 				backgroundColor: '#0066CC',
 				color: 'FFF',
@@ -23,4 +24,28 @@ const AppNavigator = createStackNavigator(
 	}
 )
 
-export default createAppContainer(AppNavigator)
+const Tabs = createBottomTabNavigator(
+	{
+		List: {screen: List},
+		About: {screen: About},
+	},
+	{
+		defaultNavigationOptions: ({navigation}) => {
+			return {
+				tabBarIcon: ({tintColor}) => {
+					const route = navigation.state.routeName
+					const name = {
+						List: 'list',
+						About: 'info-circle',
+					}[route]
+					return <Icon name={name} color={tintColor} size={22} />
+				},
+				tabBarOption: {
+					activeBackgroundColor: '#E6F0FA',
+				},
+			}
+		},
+	}
+)
+
+export default createAppContainer(Tabs)
