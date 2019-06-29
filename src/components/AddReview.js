8bb0a79f39cfda0bde478aac9e31e75a5941c533
Juliette Rapala <juliette@eventric.com>
2019-06-29 14:33:59 -0500

    Add keyboard aware scroll view.

--- src/components/AddReview.js
+++ src/components/AddReview.js
@@ -1,19 +1,60 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, TextInput } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 export class AddReview extends Component {
+	state = {
+		name: '',
+	}
 	close = () => {
 		this.props.navigation.goBack()
 	}
 	render() {
 		return (
-			<View style={styles.root}>
-				<TouchableOpacity style={styles.button} onPress={this.close}>
-					<Icon name="close" size={30} color="#0066cc" />
-				</TouchableOpacity>
-				<Text style={styles.addReview}>Add Review</Text>
-			</View>
+			<KeyboardAwareScrollView style={{ flex: 1, backgroundColor: '#FFF' }}>
+				<View style={styles.root}>
+					<TouchableOpacity style={styles.button} onPress={this.close}>
+						<Icon name="close" size={30} color="#0066cc" />
+					</TouchableOpacity>
+					<Text style={styles.addReview}>Add Review</Text>
+					<TextInput
+						style={styles.input}
+						placeholder="Name (optional)"
+						value={this.state.name}
+						onChangeText={name => this.setState({ name })}
+					/>
+					<Text style={styles.rating}>Your Rating:</Text>
+					<View style={styles.stars}>
+						{[1, 2, 3, 4, 5].map(i => {
+							return (
+								<TouchableOpacity
+									onPress={() => this.setState({ rating: i })}
+									style={styles.starButton}
+									key={i}
+								>
+									<Icon
+										name={'star'}
+										color={this.state.rating >= i ? '#FFD64C' : '#CCC'}
+										size={40}
+									/>
+								</TouchableOpacity>
+							)
+						})}
+					</View>
+					<TextInput
+						style={[styles.input, { height: 100 }]}
+						placeholder="Review"
+						value={this.state.review}
+						onChangeText={review => this.setState({ review })}
+						multiline={true}
+						numberOfLines={5}
+					/>
+					<TouchableOpacity style={styles.submitButton}>
+						<Text style={styles.submitButtonText}>Submit Review</Text>
+					</TouchableOpacity>
+				</View>
+			</KeyboardAwareScrollView>
 		)
 	}
 }
@@ -29,10 +70,45 @@ const styles = StyleSheet.create({
 		paddingHorizontal: 10,
 		paddingVertical: 20,
 	},
+	input: {
+		padding: 10,
+		marginVertical: 10,
+		marginHorizontal: 20,
+		borderColor: '#ccc',
+		borderWidth: 1,
+		borderRadius: 3,
+	},
+	rating: {
+		fontSize: 20,
+		color: 'gray',
+		textAlign: 'center',
+		marginVertical: 40,
+	},
 	root: {
 		flex: 1,
 		backgroundColor: '#fff',
 		paddingTop: 20,
+	},
+	starButton: {
+		padding: 5,
+	},
+	stars: {
+		marginBottom: 80,
+		flexDirection: 'row',
+		justifyContent: 'center',
+	},
+	submitButton: {
+		paddingVertical: 10,
+		paddingHorizontal: 20,
+		backgroundColor: '#0066cc',
+		borderRadius: 4,
+		marginVertical: 10,
+		marginHorizontal: 20,
+	},
+	submitButtonText: {
+		fontSize: 18,
+		color: '#fff',
+		textAlign: 'center',
 	},
 })
 
